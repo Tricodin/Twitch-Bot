14a90ff3dd7aaa448eb0d37ee4766d7051fcdb00
Tricodin <tricodin@gmail.com>
2015-10-26 22:48:29 -0230

    same as before
    more from before

--- sname.py
+++ sname.py
@@ -5,5 +5,13 @@ import requests
 
 dict = requests.get('https://api.twitch.tv/kraken/channels/tricodin/follows', 'Accept: application/vnd.twitchtv.v3+json')
 dict.raise_for_status()
+result = json.loads(dict.text)
+i = 0
+while (True):
+        try:
+                print result['follows'][i]['user']['name']
+                i += 1
+        except:
+                break
 
-print json.loads(dict.text)
+
